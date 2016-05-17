15eb6cb3e29aa08709551a5c8c6dc8571db04b63
Ryan Drapeau <drapeau@cs.washington.edu>
2016-05-17 09:45:51 -0700

    Pass detector fp into detect.py

--- handle_training/detect.py
+++ handle_training/detect.py
@@ -7,20 +7,22 @@ from skimage import io
 from skimage import img_as_ubyte
 from skimage import color
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print(
         "Give the path to the examples/faces directory as the argument to this "
         "program. For example, if you are in the python_examples folder then "
         "execute this program by running:\n"
-        "    ./detect.py ../examples/faces [-s]")
+        "    ./detect.py ../examples/faces ./detector.svm [-s]")
     exit()
+
 f = sys.argv[1]
+detector_fp = sys.argv[2]
 
 s = False
-if len(sys.argv) > 2 and sys.argv[2] == '-s':
+if len(sys.argv) > 3 and sys.argv[3] == '-s':
     s = True
 
-detector = dlib.simple_object_detector("detector.svm")
+detector = dlib.simple_object_detector(detector_fp)
 
 print("Processing file: {}".format(f))
 img = io.imread(f)
